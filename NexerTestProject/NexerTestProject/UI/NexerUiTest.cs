76d0d2ac2d6c9944c1ce88dc26cdc750a2f5713c
Andrei Ruskevich <andrei.ruskevich@ihsmarkit.com>
2021-08-13 10:26:39 +0300

    feat: refactoring

--- NexerTestProject/NexerTestProject/UI/NexerUiTest.cs
+++ NexerTestProject/NexerTestProject/UI/NexerUiTest.cs
@@ -1,21 +1,26 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using OpenQA.Selenium;
+using NexerTestProject.UI.TestData;
 
 namespace NexerTestProject.UI
 {
-    public class Tests
+    public class Tests : Elements
     {
+        private IWebDriver driver;
         [SetUp]
         public void Setup()
         {
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver.Navigate().GoToUrl("https://nexergroup.com/");
+            driver.Manage().Window.Maximize();
         }
 
         [Test]
-        public void Test1()
+        public void NexerMainPage_Menu_VerifyMenuOptions()
         {
-            Assert.Pass();
+            driver.FindElement(MenuButton).Click();
+            var actualName = driver.FindElement(DesigAndCommunicationTitle).Text;
+            Assert.AreEqual(ExpectedSectionName, actualName);
         }
     }
 }
---
+++ NexerTestProject/NexerTestProject/UI/TestData/Elements.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium;
+
+
+namespace NexerTestProject.UI.TestData
+{
+    public class Elements
+    {
+        public string ExpectedSectionName = "DESIGN & COMMUNICATION";
+        public static By MenuButton = By.CssSelector("#nav-hamburger-toggle");
+        public static By DesigAndCommunicationTitle = By.XPath("//*[contains(text(),'Design & Communication')]");
+    }
+}
