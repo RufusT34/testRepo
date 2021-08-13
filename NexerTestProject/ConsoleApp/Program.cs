a9fd0571746fe22b244f3573f13fb14ad30dc515
Andrei Ruskevich <andrei.ruskevich@ihsmarkit.com>
2021-08-13 13:09:06 +0300

    feat: add console app

---
+++ NexerTestProject/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static int calc(int n)
+        {
+            int calcResult;
+            calcResult = 1;
+            for (int i = 1; i <= n; i++)
+                calcResult = calcResult * i;
+            return calcResult;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculation factorial.");
+            Console.Write("Enter a value - ");
+            int value = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}! = {1}", value, calc(value));
+        }
+    }
+}
